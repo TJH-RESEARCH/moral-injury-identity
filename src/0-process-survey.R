71685b295940ea1d729d56e572578fdd3b5607df
All Caps <allcaps@air-2.local>
2024-02-14 22:26:59 -0500

    about to delete the main file i used that was split into diff screening files

--- src/0-process-survey.R
+++ src/0-process-survey.R
@@ -22,14 +22,39 @@ source(here::here('src/01_config/functions/function-undo-reverse-codes.R'))
 source(here::here('src/01_config/functions/function-calculate-longstring.R'))
 
 ### b. Apply Screeners -------------------------------------------------------
+
+# Set cut offs
+
+# Survey host flagged for removal
 source(here::here('src/02_dataprep/7-screening/screen-survey-host.R'))
+
+# Honeypot quesitons; Qualtrics bot metrics
 source(here::here('src/02_dataprep/7-screening/screen-bots-dups.R'))
+
+# Attention checks: 2 items
 source(here::here('src/02_dataprep/7-screening/screen-attention-checks.R'))
+
+# 1 validity check item; Inconsistencies across demographic items; Free response screening
 source(here::here('src/02_dataprep/7-screening/screen-validity-checks.R'))
+
+# Longstring by scale
+## Straightlined the MIOS with score higher more than 0
+## Straightlined the M2CQ with a score more than 0
+# BIIS = 17 items total
+# MCARM = 21 items - <19
+# SCC = 12 items - <10
 source(here::here('src/02_dataprep/7-screening/screen-longstring.R'))
+
+# filter(evenodd < 0)
 source(here::here('src/02_dataprep/7-screening/screen-evenodd.R'))
+
+# psychsyn critval 0.76; psychant critval = -0.62; filter(psychsyn > 0)
 source(here::here('src/02_dataprep/7-screening/screen-synant.R'))
+
+## Set the cutoff - Three and a half standard deviations from the mean of the data:
 source(here::here('src/02_dataprep/7-screening/screen-avgstring.R'))
+
+
 
 ### c. Save a table of survey exclusions and response time
 source(here::here('src/04_appendix/survey-exclusions.R'))
